a5a2eed84fb0a7304b905ef40ad48611218cfe93
Uwe Riegel <uwe@caseris.intern>
2020-03-10 13:09:57 +0100

    Tester, solution

---
+++ AppSettings/Settings.fs
@@ -0,0 +1,19 @@
+﻿module Settings
+open System.Runtime.InteropServices
+open System.IO
+open System
+open Newtonsoft.Json.Linq
+
+let mutable private subPath = ""
+let mutable data = JObject()
+
+
+let private path = Lazy<string>.Create <| fun () -> 
+    if subPath = "" then failwith "AppSettings not initialized"
+    else Path.Combine (Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), subPath)
+
+let initialize organization application = 
+    subPath <- Path.Combine (organization, application)
+
+let get () =
+    data
---
+++ Tester/Program.fs
@@ -0,0 +1,9 @@
+﻿open System
+open Newtonsoft.Json.Linq
+
+[<EntryPoint>]
+let main argv =
+    Settings.initialize "URiegel" "Tester"
+    let settings = Settings.get ()
+    settings.["Affe"] <- JToken()
+    9
